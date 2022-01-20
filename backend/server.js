6e6a1874b95e4612473d0b8e8a24892d8d0b6f07
Cha <progat24.e@gmail.com>
2022-01-20 01:57:05 +0100

    add new routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 const matchQuiz = require('./models/matchquiz');
 const userAnswer = require('./models/answer');
+const user = require('./models/user');
 
 mongoose.connect('mongodb+srv://petmatch-admin:techlab2122@cluster0.9nbuq.mongodb.net/petmatch', {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection;
@@ -12,8 +14,8 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-// app.use(express.urlencoded({extended: true}))
-// app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // get all questionnaires from database
 app.get('/matchquiz', async(req, res) => {
@@ -21,10 +23,34 @@ app.get('/matchquiz', async(req, res) => {
     res.json(questions);  
 });
 
+// show old result for log-in user
+app.get('/showsavedresult', async(req, res) => {
+    const userID = req.body.userID;
+    const savedAnswer = await userAnswer.find({userID: userID});
+    res.json(savedAnswer); 
+})
+
 // test answer collection
-app.get('/answer', async(req, res) => {
-    const savedAnswer = await userAnswer.find();
-    res.json(savedAnswer);
+app.post('/submitanswer', async (req, res) => {
+    // const userID = req.body.userID;
+    // const userAnswer = req.body.chosenAnswer;
+    // const allAnswer = [];
+    // for (let choices of userAnswer){
+    //     allAnswer.push(choices["a"]);
+    // };
+        
+    res = await axios.post('http://omaistat.pythonanywhere.com/predict', [{"1": 1, "2": 2, "3": 4, "4": 3, "5": 2, "6": 5, "7": 4, "8": 3, "9": 4, "10": 5, "11": 3, "12": 4, "13": 5, "14": 2, "15": 3, "16": 4, "17": 5, "18": 4, "19": 3}]
+        // userID: userID,
+        // allChosenAnswer: allAnswer
+    );
+
+    //console.log(userID);
+    res.json(res);
+})
+
+// get cat result
+app.get('/result', (req, res) => {
+    //get result from above route to query database to get all cat info
 })
 
 app.listen(3001, () => {
