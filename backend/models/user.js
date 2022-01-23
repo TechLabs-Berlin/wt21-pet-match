d64ce3d2013a919c0f465812dad9d2addb084488
Cha <progat24.e@gmail.com>
2022-01-23 01:03:40 +0100

    edit user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema  = new mongoose.Schema({
     email: {
         type: String,
-        unique: true
+        unique: true,
+        sparse: true
     },
     password: String,
     memberAccount: Boolean,
