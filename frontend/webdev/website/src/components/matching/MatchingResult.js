7bd4152ec885234ff373a6620de2e7aafde6b590
BR-VB <roeck.brigitta@gmail.com>
2022-01-25 04:55:53 +0100

    commented out usage of file MatchingResult.css

--- frontend/webdev/website/src/components/matching/MatchingResult.js
+++ frontend/webdev/website/src/components/matching/MatchingResult.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import matchingResultCSS from './MatchingResult.css';
+//import matchingResultCSS from './MatchingResult.css';
 
 const MatchingResult = () => {
     return <div>Pet Match - Matching - Results</div>;
